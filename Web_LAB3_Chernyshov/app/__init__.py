33ede467f2b57d12a17f6fc21a1d9fd261735d5b
Ubermensch <ubermensch1937@bk.ru>
2025-04-23 19:05:52 +0300

    bozhe

--- Web_LAB3_Chernyshov/app/__init__.py
+++ Web_LAB3_Chernyshov/app/__init__.py
@@ -1,5 +1,13 @@
+import logging
 from flask import Flask
 
 app = Flask(__name__)
+
+# Настройка логирования
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s %(levelname)s %(message)s',
+    filename='app.log'
+)
 
 from . import routes  # импортируем маршруты из routes.py 
--- Web_LAB3_Chernyshov/app/routes.py
+++ Web_LAB3_Chernyshov/app/routes.py
@@ -1,10 +1,35 @@
 import random
 from functools import lru_cache
-from flask import render_template, request, redirect, url_for, flash, abort
+from flask import render_template, request, redirect, url_for, flash, session, abort
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from faker import Faker
 from . import app
 
 fake = Faker('ru_RU')
+
+# Настройка Flask-Login
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+# Конфигурация приложения
+app.config['SECRET_KEY'] = 'your-secret-key'  # для работы с сессиями
+
+# Простая модель пользователя
+class User(UserMixin):
+    def __init__(self, username):
+        self.id = username
+
+# Словарь для хранения пользователей (в реальном приложении должна быть база данных)
+users = {
+    'admin': {'password': 'admin123'}
+}
+
+@login_manager.user_loader
+def load_user(username):
+    if username not in users:
+        return None
+    return User(username)
 
 images_ids = ['7d4e9175-95ea-4c5f-8be5-92a6b708bb3c',
               '2d2ab7df-cdbc-48a8-a936-35bba702def5',
@@ -41,6 +66,40 @@ def posts_list():
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        
+        if username in users and users[username]['password'] == password:
+            user = User(username)
+            login_user(user)
+            return redirect(url_for('secret'))
+        else:
+            flash('Неверное имя пользователя или пароль')
+    
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/secret')
+@login_required
+def secret():
+    return render_template('secret.html')
+
+@app.route('/visits')
+def visits():
+    if 'visits' not in session:
+        session['visits'] = 1
+    else:
+        session['visits'] = session['visits'] + 1
+    return render_template('visits.html', visits=session['visits'])
 
 @app.route('/posts')
 def posts():
